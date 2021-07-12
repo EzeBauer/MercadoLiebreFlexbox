56a7c0761c3ae687bcac6e23b2a9364f13f70f63
EzeBauer <83975962+EzeBauer@users.noreply.github.com>
2021-07-12 06:57:16 -0300

    flexbox terminado

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ app.listen(port, () =>
 );
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")))
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, "./views/register.html")))
 
 app.use(express.static('public'));
